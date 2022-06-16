0d8d88073d1aa84ee821cf6c7e7d215a24d5a38a
personaseria23 <charliespotifai@gmail.com>
2022-06-15 23:47:01 -0400

    geolocalización kinda ready

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Geolocation } from '@capacitor/geolocation';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+
+  btnGeo(){
+    console.log("nacho chupalo");
+  }
+
+ async getCurrentLocation(){
+    try{
+      const cordenadas = await Geolocation.getCurrentPosition();
+      console.log(`Current position:`, cordenadas);
+    }catch(e){
+      console.log(e);
+    }
+  }  
+
+
 
 }
